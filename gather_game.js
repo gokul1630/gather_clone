c8af8f88f849271dfd53a3e4f3b357ee737e475f
gokul1630 <gokulgp3167@gmail.com>
2023-06-24 12:46:44 +0530

    finally dynamic users :)

--- gather_game.js
+++ gather_game.js
@@ -4,49 +4,53 @@ const sendBtn = document.querySelector('#submitBtn');
 const chatBox = document.querySelector('#chatbox');
 let user = JSON.parse(window.localStorage.getItem("user"))
 
-const currentUser = user?.user
+window.onload=()=>{
+  if(!user) window.location.href='index.html'
+}
+
+const currentUser = user?.userId
 let peer
+var map;
+const players = {}
+var cursors;
+let users = []
+
 
 messageBox.placeholder = `Hi ${user?.user}, Type Something...`
 
 const messageClass = "flex w-auto"
-const socket = io("ws://localhost:8081");
-
+const socket = io("ws://localhost:8081"); 
 
 var config = {
-  type: Phaser.CANVAS,
-  canvas: document.getElementById('game-area'),
-  height: window.innerHeight,
-  width: window.innerWidth - 20,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 },
-      debug: false,
-    },
+      type: Phaser.CANVAS,
+      canvas: document.getElementById('game-area'),
+      height: window.innerHeight,
+      width: window.innerWidth - 20,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0 },
+          debug: false,
+        },
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update,
+    preload,
+    create,
+    update,
   },
 };
 
 const game = new Phaser.Game(config);
 
-var map;
-let players = [];
-var cursors;
-let physics, cameras, treesLayer
 
 class Player {
   physics
   cameras
   player
   user
-  constructor (scene, camera, treesLayer, x, y, user) {
+  constructor(scene, camera, treesLayer, x, y, user) {
     if (scene && camera && treesLayer && user) {
-
+      
       this.user = user
       this.cameras = camera
       this.physics = scene
@@ -57,32 +61,32 @@ class Player {
       this.cameras.main.startFollow(this.player);
     }
   }
-
+  
   update() {
     if (this.user === currentUser) {
       this.player.setVelocityY(0);
       this.player.setVelocityX(0);
-
+      
       if (cursors.up.isDown == true && cursors.left.isDown == true) {
         this.player.setVelocity(-100, -100); //-100, -100
         this.player.anims.play('move-left', true);
       }
-
+      
       else if (cursors.up.isDown == true && cursors.right.isDown == true) {
         this.player.setVelocity(100, -100);  //100, -100
         this.player.anims.play('move-right', true);
       }
-
+      
       else if (cursors.down.isDown == true && cursors.left.isDown == true) {
         this.player.setVelocity(-100, 100); //-100, 100
         this, player.anims.play('move-left', true);
       }
-
+      
       else if (cursors.down.isDown == true && cursors.right.isDown == true) {
         this.player.setVelocity(100, 100); //100, 100
         this.player.anims.play('move-right', true);
       }
-
+      
       else if (cursors.up.isDown == true) {
         this.player.setVelocityY(-100);
         this.player.anims.play('move-up', true);
@@ -118,15 +122,19 @@ function preload() {
 }
 
 function create() {
+  socket.emit('newUser',currentUser)
   map = this.make.tilemap({ key: "map", tileWidth: 100, tileHeight: 100 });
   var tileset = map.addTilesetImage("game_tiles", "tiles");
   var groundLayer = map.createLayer("GroundLayer", tileset, 0, 0);
   var treesLayer = map.createLayer("TreesLayer", tileset, 0, 0);
   treesLayer.setCollisionBetween(30, 31, 32, 33, 34, 35, 39, 40, 41, 42, 43, 44, 48, 49, 50, 51, 52, 53);
-
-
+  
+  if(this.physics && this.cameras && treesLayer){
+    players[currentUser] = new Player(this.physics, this.cameras, treesLayer, (Math.floor(Math.random()*100)) + 300, 500, currentUser);
+  }
+  
   cursors = this.input.keyboard.createCursorKeys();
-
+  
   this.anims.create({
     key: 'move-left',
     frames: this.anims.generateFrameNumbers('player-move', { start: 15, end: 18 }),
@@ -134,7 +142,7 @@ function create() {
     repeat: 0,
     duration: 100
   });
-
+  
   this.anims.create({
     key: 'move-up',
     frames: this.anims.generateFrameNumbers('player-move', { start: 5, end: 8 }),
@@ -142,7 +150,7 @@ function create() {
     repeat: 0,
     duration: 100
   });
-
+  
   this.anims.create({
     key: 'move-right',
     frames: this.anims.generateFrameNumbers('player-move', { start: 10, end: 13 }),
@@ -150,8 +158,8 @@ function create() {
     repeat: 0,
     duration: 100
   });
-
-
+  
+  
   this.anims.create({
     key: 'move-down',
     frames: this.anims.generateFrameNumbers('player-move', { start: 0, end: 3 }),
@@ -159,27 +167,25 @@ function create() {
     repeat: 0,
     duration: 100
   });
-
+  
   this.anims.create({
     key: 'down-idle',
     frames: this.anims.generateFrameNumbers('player-idle', { start: 0, end: 1 }),
     frameRate: 3,
     repeat: -1
   });
-
-
+  
+  
   // Set up the camera to follow the player
   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  players = [
-    new Player(this.physics, this.cameras, treesLayer, 300, 500, "gokul"),
-    new Player(this.physics, this.cameras, treesLayer, 400, 500, "anuvindh"),
-    new Player(this.physics, this.cameras, treesLayer, 500, 500, "anu1"),
-    new Player(this.physics, this.cameras, treesLayer, 600, 500, "anu2"),
-    new Player(this.physics, this.cameras, treesLayer, 700, 500, "anu3"),
-    new Player(this.physics, this.cameras, treesLayer, 800, 500, "anu4")
-  ]
-
+  
+  socket.on('newUser',async (user) => {
+    if(this.physics && this.cameras && treesLayer && !players[user]){
+      players[user] = new Player(this.physics, this.cameras, treesLayer, (Math.floor(Math.random()*100)) + 300 , 500, user)
+    }
+  })
 }
+
 
 let collisionFlag = false
 let collision = false
@@ -212,76 +218,78 @@ const moveDirection = () => {
 }
 
 function update() {
-  players.forEach((player) => {
+  Object.values(players).forEach((player) => {
     player.update()
     // player.player.setCollideWorldBounds(true)
   })
-
+  
   const collidedUsers = new Set()
 
-  players.forEach(playerOne => players.forEach(playerTwo => {
+if(Object.values(players).length){
+  Object.values(players).forEach(playerOne => Object.values(players).forEach(playerTwo => {
     if (playerOne.player !== playerTwo.player && checkCollision(playerOne.player, playerTwo.player)) {
       collision = checkCollision(playerOne.player, playerTwo.player)
       collidedUsers.add(playerOne.player.playerId)
       collidedUsers.add(playerTwo.player.playerId)
     }
   }))
-
+}
+  
   const users = [...collidedUsers.keys()].filter(user => user !== currentUser).map(user => ({ from: currentUser, to: user }))
-
+  
   if (collision !== collisionFlag) {
     collisionFlag = collision;
     socket.on(currentUser, (event) => {
       createMessage(event.message, event.user)
     })
-
+    
     // peer = new Peer({
-    //   host: "localhost",
-    //   port: 9000,
-    //   path: "/myapp",
-    // });
-    sendBtn.addEventListener('click', () => {
-      if (messageBox.value) {
-        socket.emit('chat', { message: messageBox.value, users })
-        createMessage(messageBox.value, currentUser)
-      }
-    })
-    if (!collision) {
-      sendBtn.removeEventListener('click', {})
-    } else {
-      // peer.on('open', (peerId) => {
-      //   socket.emit("peer-id", { peerId, currentUser })
-      // })
-
-      // socket.on('peer-id', (event) => {
-      //   if (currentUser !== event.currentUser) {
-      //     if (hasUserMedia()) {
-      //       navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      //         || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-      //       navigator.getUserMedia({ video: true, audio: true }, function (stream) {
-      //         const video = document.querySelector('video');
-      //         video.srcObject = stream;
-      //         peer.call(event.peerId, stream)
-
+      //   host: "localhost",
+      //   port: 9000,
+      //   path: "/myapp",
+      // });
+      sendBtn.addEventListener('click', () => {
+        if (messageBox.value) {
+          socket.emit('chat', { message: messageBox.value, users })
+          createMessage(messageBox.value, currentUser)
+        }
+      })
+      if (!collision) {
+        sendBtn.removeEventListener('click', {})
+      } else {
+        // peer.on('open', (peerId) => {
+          //   socket.emit("peer-id", { peerId, currentUser })
+          // })
+          
+          // socket.on('peer-id', (event) => {
+            //   if (currentUser !== event.currentUser) {
+              //     if (hasUserMedia()) {
+                //       navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+                //         || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+                
+                //       navigator.getUserMedia({ video: true, audio: true }, function (stream) {
+                  //         const video = document.querySelector('video');
+                  //         video.srcObject = stream;
+                  //         peer.call(event.peerId, stream)
+                  
       //       }, function (err) {
       //         console.log(err)
       //       });
-
+      
       //     }
       //   } else {
-      //     if (hasUserMedia()) {
+        //     if (hasUserMedia()) {
       //       navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
       //         || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
+      
       //       navigator.getUserMedia({ video: true, audio: true }, function (stream) {
-      //         const video = document.querySelector('video');
-      //         video.srcObject = stream;
-      //         peer.on('call', (call) => {
-      //           call.answer(event.peerId)
-      //         })
-      //       }, function (err) {
-      //         console.log(err)
+        //         const video = document.querySelector('video');
+        //         video.srcObject = stream;
+        //         peer.on('call', (call) => {
+          //           call.answer(event.peerId)
+          //         })
+          //       }, function (err) {
+            //         console.log(err)
       //       });
       //     }
       //   }
@@ -293,17 +301,17 @@ function update() {
   } else {
     chatBox.className = 'hidden'
   }
-
+  
   if (cursors.up.isDown || cursors.down.isDown || cursors.left.isDown || cursors.right.isDown) {
-    players.forEach(player => {
+    Object.values(players).forEach(player => {
       if (player.player.playerId === currentUser) {
-        socket.emit('movement', { x: player.player.x, y: player.player.y, player: player.player.x, player: player.player.playerId, move: moveDirection() })
+        socket.emit('movement', { x: player.player.x, y: player.player.y, player: player.player.playerId, move: moveDirection() })
       }
     })
   }
 }
 socket.on(`movement`, (event) => {
-  players.forEach(player => {
+  Object.values(players).forEach(player => {
     const playerId = player.player.playerId;
     if (playerId === event.player && event.player !== currentUser) {
       player.player.setPosition(event.x, event.y)
@@ -318,17 +326,31 @@ function checkCollision(object1, object2) {
   var distanceX = object1.x - object2.x;
   var distanceY = object1.y - object2.y;
   var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
+  
   // Check if the distance is less than the sum of the object sizes (assuming square objects)
   var objectSize = object1.width; // Assuming both objects have the same size
   var collisionDistance = objectSize * 2;
-
+  
   return distance < collisionDistance
 }
 
 
 function hasUserMedia() {
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-    || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  || navigator.mozGetUserMedia || navigator.msGetUserMedia;
   return !!navigator.getUserMedia;
 }
+
+window.onclose=()=>{
+  socket.emit('removeUser', currentUser)
+}
+
+const clr = document.querySelector(".clr");
+const userss = document.querySelector("#user");
+
+userss.innerText = JSON.parse(window.localStorage.getItem('user')).userId
+
+clr.addEventListener('click',()=>{
+  localStorage.clear()
+  window.location.reload()
+})
